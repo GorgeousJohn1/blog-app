73d2a9cb0f79e71c2f43b7d2cc4e03b733faaadc
Ivan Voronzhev <gorgeousjohn1@gmail.com>
2024-09-24 19:08:15 +0500

    hash-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import store from './app/store';
 import App from './App';
 import './index.css';
@@ -9,11 +9,11 @@ import './index.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
